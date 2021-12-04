e9d7a9e311a2ddcaa82eb7702fef283f52709310
Steven <stntsai93@gmail.com>
2021-12-04 12:01:00 -0500

    create local copy of contract

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import web3 from './web3.js';
 
 class App extends React.Component {
   render() {
+    web3.eth.getAccounts().then(console.log)
     return (
       <div className="App">
         <header className="App-header">
