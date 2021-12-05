9608f62744ba7fe7531e73d32ddb7cfea6240b2c
Steven <stntsai93@gmail.com>
2021-12-05 13:35:07 -0500

    pickWinner

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import web3 from './web3.js';
+import lottery from "./lottery";
 
 class App extends React.Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: ''
+  };
+
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+  
+  onSubmit= async event=>{
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction to enter the lottery..'})
+    console.log(accounts)
+    await lottery.methods.enter().send({
+      from : accounts[0],
+      value: web3.utils.toWei(this.state.value,'ether')
+    });
+
+    this.setState({ message: 'You are now in the pool!'})
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'waiting on transaction success...'});
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+    this.setState({ message: "The winner has been picked! Check your account and if that's you:) "})
+  }
+
   render() {
-    web3.eth.getAccounts().then(console.log)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          This contract is managed by {this.state.manager} .
+          There are currently {this.state.players.length} people in the pool 
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <h3>Try your luck!</h3>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        
+        <hr/>
+
+        <h4> Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
--- src/lottery.js
+++ src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const address = '0xdCBa2e593E16Aff17cB466B8F22e8c2bf1797Fbb';
+const address = '0x784BDD0Fa3c491ba2b93A4A215c2E07ec296F2B3';
 
 const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
 
